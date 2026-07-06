8c60a0f883e6dda2fcae4920e88efa832e71e5c3
garridotomas <tomasgf13@gmail.com>
2026-07-06 14:18:33 -0700

    se sellan los cambios y se agrega un main para probar el codigo

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-public class main {
+import vista.VentanaPrincipal;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Punto de entrada de la aplicación.
+ *
+ * POR QUÉ invokeLater():
+ * Swing no es thread-safe. invokeLater() encola la construcción
+ * de la UI en el Event Dispatch Thread (EDT), garantizando que
+ * todo Swing corra en el hilo correcto desde el inicio.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(VentanaPrincipal::new);
+    }
 }
